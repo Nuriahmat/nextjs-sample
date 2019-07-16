538e66f6075007fb44ebd8f248ddc53115730ac8
nueraihemaiti-k <nueraihemaiti-k@dmm.com>
2019-07-16 17:14:16 +0900

    add typescript

---
+++ .next/static/webpack/static/development/pages/index.js.5be6150c73db9c14d1e6.hot-update.js
@@ -0,0 +1,42 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.tsx":
+/*!*************************!*\
+  !*** ./pages/index.tsx ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _assets_css_style_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../assets/css/style.css */ "./assets/css/style.css");
+/* harmony import */ var _assets_css_style_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_assets_css_style_css__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/Users/nueraihemaiti-k/Dev/42Tokyo/nextjs-sample/pages/index.tsx";
+
+
+
+var IndexPages = function IndexPages() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    className: _assets_css_style_css__WEBPACK_IMPORTED_MODULE_1___default.a.hoge,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, "Welcome to", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("b", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, " Next.js "), "for TypeScript!");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (IndexPages);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.5be6150c73db9c14d1e6.hot-update.js.map
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import css from '../assets/css/style.css';
+
+const IndexPages = () => {
+  return (
+    <h1 className={css.test}>
+      Welcome to
+      <b> Next.js </b>
+      for TypeScript!
+    </h1>
+  );
+};
+
+export default IndexPages;
